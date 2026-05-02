559ca790aab594f46693ad974638ffaf25fc9f97
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:20:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# MAX6675 Module
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/max6675?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-sensor-lightgrey.svg)
+
+K-type thermocouple temperature measurement module based on MAX6675, the measurement range is 0 ~ 1024 ℃
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,67 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'max6675.name': 'MAX6675 Module',
+            'max6675.description': 'K-type thermocouple temperature measurement module based on MAX6675, the measurement range is 0 ~ 1024 ℃'
+        },
+        'ru': {
+            'max6675.name': 'MAX6675 Module',
+            'max6675.description': 'K-type thermocouple temperature measurement module based on MAX6675, the measurement range is 0 ~ 1024 ℃'
+        },
+        'zh-cn': {
+            'max6675.name': 'MAX6675 Module',
+            'max6675.description': 'K-type thermocouple temperature measurement module based on MAX6675, the measurement range is 0 ~ 1024 ℃'
+        },
+        'zh-tw': {
+            'max6675.name': 'MAX6675 Module',
+            'max6675.description': 'K-type thermocouple temperature measurement module based on MAX6675, the measurement range is 0 ~ 1024 ℃'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'MAX6675_CATEGORY': 'MAX6675',
+            'MAX6675_INIT': 'init max6675 pin DO %1 CLK %2 CS %3',
+            'MAX6675_READTEMPERATURE': 'max6675 read temperature %1'
+        },
+        'ru': {
+            'MAX6675_CATEGORY': 'MAX6675',
+            'MAX6675_INIT': 'init max6675 pin DO %1 CLK %2 CS %3',
+            'MAX6675_READTEMPERATURE': 'max6675 read temperature %1'
+        },
+        'zh-cn': {
+            'MAX6675_CATEGORY': 'MAX6675',
+            'MAX6675_INIT': 'init max6675 pin DO %1 CLK %2 CS %3',
+            'MAX6675_READTEMPERATURE': 'max6675 read temperature %1'
+        },
+        'zh-tw': {
+            'MAX6675_CATEGORY': 'MAX6675',
+            'MAX6675_INIT': 'init max6675 pin DO %1 CLK %2 CS %3',
+            'MAX6675_READTEMPERATURE': 'max6675 read temperature %1'
+        }
+    }
+};
